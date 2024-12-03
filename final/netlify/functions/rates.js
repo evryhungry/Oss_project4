56d6795aff63cb262395defec170b34e5e35f276
Dohyun Kim <143365353+evryhungry@users.noreply.github.com>
2024-12-03 15:41:46 +0900

    Add the log

--- final/netlify/functions/rates.js
+++ final/netlify/functions/rates.js
@@ -11,11 +11,7 @@ exports.handler = async (event) => {
     let response;
     if (method === "POST") {
       response = await axios.post(`${API_BASE_URL}${path}`, body, {
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          appkey: APP_KEY,
-          appsecret: APP_SECRET,
-        },
+        
       });
     } else if (method === "GET") {
       response = await axios.get(`${API_BASE_URL}${path}`, {
